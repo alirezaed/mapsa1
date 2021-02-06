0cbd596dd55ddc1f1685fc407b3179b43f8bad35
ali edalat <aliedalat.pr@gmail.com>
2021-02-06 18:54:12 +0330

    change styles.

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ rootElement.style.width="200px"
 rootElement.style.height="200px"
 rootElement.style.backgroundColor="red"
 const level1 = document.getElementById('level1');
-level1.style.width="200px"
-level1.style.height="200px"
 const level2 = document.getElementById('level2');
-level2.style.width="200px"
-level2.style.height="200px"
+
 
 rootElement.addEventListener('click',(e)=>{
+    e.stopPropagation();
     console.log('root');
 });
 level1.addEventListener('click',(e)=>{
